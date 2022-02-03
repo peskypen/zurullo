eead81c269af859a81f059d9e3fe82389eade3fe
shieldfinanceavax <aguacatesfrescosdelaindia@gmail.com>
2022-02-03 14:48:40 +0100

    .

---
+++ _next/static/u0ApqVDHmXIRRUcqcpHAl/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/692-4eb9df1d2f455c27602d.js","static/chunks/pages/index-4dfcef9305573707efb0.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
